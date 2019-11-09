d041afe3b5b8f7b4df6633159d691a276e841d7d
Aureamh <57450766+Aureamh@users.noreply.github.com>
2019-11-09 14:34:50 +0100

    Arreglo de los errores en los Test: - ProbarClasesInterfazGenericaYAnidadas - ProbarGenericos

--- 01_IntroJava/test/com/vn/introjava/tests/poo/ProbarGenericos.java
+++ 01_IntroJava/test/com/vn/introjava/tests/poo/ProbarGenericos.java
@@ -5,6 +5,8 @@
  */
 package com.vn.introjava.tests.poo;
 
+import com.vn.introjava.poo.vehiculos.Coche;
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,10 +15,122 @@ import static org.junit.Assert.*;
  * @author Equipo 13
  */
 public class ProbarGenericos {
+
+
+//    @Test
+//    public void testearMetodosGenericos() {
+//        ArrayList<Integer> listIn = new ArrayList<Integer>();
+//        int tres = 3, cinco = 5;
+////        insertarIntegerDuplicado(listIn, tres);
+////        insertarIntegerDuplicado(listIn, cinco);
+//        insertarDuplicado(listIn, tres);
+//        insertarDuplicado(listIn, cinco);
+//        System.out.println("Tres sigue siendo " + tres);
+//
+//        for (Integer integer : listIn) {
+//            System.out.println(" - " + integer);
+//        }
+//   }
     
-    public ProbarGenericos() {
+   @Test
+    public void testearClaseAnidada() {
+        // Las clases anidadas generan su propio fichero bytecodes
+        ProbarClaseInterfazGenericaYAnidadas.ClasePequePos c;
+        c = new ProbarClaseInterfazGenericaYAnidadas.ClasePequePos(1f, 2f);
+    }
+            
+    @Test
+    public void testearMetodosGenericos() {
+        ArrayList<Integer> listIn = new ArrayList<Integer>();
+        int tres = 3, cinco = 5;
+        insertarDuplicado(listIn, tres);
+        insertarDuplicado(listIn, cinco);
+        System.out.println("Tres sigue siendo " + tres);
+        for (Integer integer : listIn) {
+            System.out.println(" - " + integer);
+        }
+            
+        //Inferir: Infiere, deduce el tipo por su declaración.
+        ArrayList<String> listStr = new ArrayList<>();
+//        insertarStringDuplicado(listStr, "tres");
+//        insertarStringDuplicado(listStr, "cinco");
+        insertarDuplicado(listStr, "tres");
+        insertarDuplicado(listStr, "cinco");
+
+        for (String unString : listStr) {
+            System.out.println(" ** " + unString);
+        }
+        
+    //Ejercicio 1:  Método genérico que reciba un ArrayList de Coche.
+        ArrayList<Coche> listCoche = new ArrayList<>();
+        insertarDuplicado(listCoche, new Coche ("Seat"));
+        insertarDuplicado(listCoche, new Coche ("BMW"));
+        insertarDuplicado(listCoche, new Coche ("Ford"));
+    
+    //Ejercicio 2: Método genérico que reciba un ArrayList<Tipo>, que lo muestre
+    //(foreach) y devuelva el último valor. 
+    //(Otra forma más abajo)
+        for (Coche miCoche : listCoche) {
+            System.out.println(miCoche); //Así recorro lo que tengo en el ArrayList
+        }
+        
+        System.out.println("Último valor = " + listCoche.get(listCoche.size()-1));
+        //Ponemos -1 porque en los array empieza a enumerar en posición 0.
+    
+        Integer ultInt = mostrarYdevolverUltimo(listIn);
+        String ultStr = mostrarYdevolverUltimo(listStr);
+        Coche ultCoche = mostrarYdevolverUltimo(listCoche);
+        assertEquals((int)ultInt, 5); //cambio Integer a int
+        assertEquals(ultInt, (Integer)5); //cambio int a Integer
+        //Sino da error porque Integer es un objeto e int una variable primitiva
+        //entonces no se pueden comparar, por tanto, hay que cambiar uno a otro.
+        assertEquals(ultStr, "cinco");
+        assertEquals(ultCoche.getMarca(), "Ford");
+    
+    }
+    
+    
+    //Entre < > ponemos el nombre del tipo genérico. Esto convierte el método
+    //en un "método genérico".
+    static <Tipo> void insertarDuplicado(ArrayList<Tipo> listaGenerica, Tipo objeto) {
+        listaGenerica.add(objeto); //Así se añaden los datos al ArrayList
+        listaGenerica.add(objeto);
+        System.out.println(objeto + " añadido dos veces");
+        //Esto no afecta fuera del método.
+        
+        if(objeto instanceof Integer) {
+            Integer i = (Integer) objeto; //Conversión explícita
+            i = i+2;
+        }
+        listaGenerica = null;
+    }
+    
+    //Otra forma de hacer el Ejercicio 2, hecho por el profesor.
+    static<T> T mostrarYdevolverUltimo(ArrayList<T> lista) {
+        
+        for (T infusion : lista) {
+            System.out.println(">> " + infusion.toString()); //Así recorro lo que tengo en el ArrayList
+        }
+        if (lista.size() > 0)
+            return lista.get(lista.size()-1);
+        else
+            return null;  
     }
 
-    // @Test
-    // public void hello() {}
+    /* Métodos NO genéricos */
+//    static void insertarIntegerDuplicado(ArrayList<Integer> listaInt, int i) {
+//        listaInt.add(i);
+//        listaInt.add(i);
+//        System.out.println(i + " añadido dos veces");
+//        i = i + 10;
+//    }
+//
+//    static void insertarStringDuplicado(ArrayList<String> listaString, String str) {
+//
+//        listaString.add(str);
+//        listaString.add(str);
+//        System.out.println(str + " añadido dos veces");
+//        str = str + "10";
+//    }
+
 }
