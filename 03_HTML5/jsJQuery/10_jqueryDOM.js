db360a2392c4a776279515feffee997fbe5722e8
Aurea <aurea.mene.h@gmail.com>
2019-11-20 13:30:56 +0100

    jQuery DOM, animaciones y eventos

--- 03_HTML5/jsJQuery/10_jqueryDOM.js
+++ 03_HTML5/jsJQuery/10_jqueryDOM.js
@@ -1,5 +1,5 @@
 //jQuery(...) se abrevia como $(...)
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
     jQuery("section").append("<div><p>LEYENDA de lo que sea</p></div>");
 
@@ -9,23 +9,52 @@ jQuery(document).ready(function() {
     //Añadir quinta columna:
     $(".articulo").append("<p>UN PÁRRAFO EN CADA ELEMENTO con class = 'articulo'</p>");
     //Moficamos un atributo de esa columna:
-    $("td:nth-child(5)").attr("style","background-color: yellow; color: green");
+    $("td:nth-child(5)").attr("style", "background-color: yellow; color: green");
     $("td:nth-child(5)").click(() => {
         alert('un click desde jQ');
     });
 
-    $("#otro_menu").append('<p>.....</p><br></br>');
-    $("#otro_menu").click(function() {
-        //$('#otro_menu').fadeOut();
-        $(this).slideUp();
+    $("#otro_menu").html('<h2>Menú de artículos</h2><br></br>');
+
+    $("article").hide();
+    $("article").each(function (index) {
+        let idArticulo = "articulo_29juR_" + index
+        $(this).attr("id", idArticulo);
+        //$("#otro_menu").append("<a href='#" + idArticulo + "' id='enlace" + index + "' class='enlace-articulo'>Articulo nº"
+        //+ index + "</a>");
+        $("#enlace" + index).click(function () {
+            $("#" + idArticulo).show();
+        });
+        
+        // Para que al pulsar un artículo se oculte otro ya abierto.
+        $("#otro_menu").append("<a id='enlace" + idArticulo + "' " + "' class='enlace-articulo'>Articulo nº"
+        + index + "</a>");
+        $("#enlace" + idArticulo).click(function(){
+            if (typeof(articuloActivo) ==="undefined"){
+                $("#" + idArticulo).slideDown(1500);
+            }else {
+                $(articuloActivo).slideUp(800,function() {
+                    $("#" + idArticulo).slideDown(1500);
+                });
+            }
+            articuloActivo = $("#" + idArticulo);
+        })
+        
+        /* Para que queden fijos los artículos al pulsar
+        $("#enlace" + idArticulo).click(function(){
+            $("article").slideUp(800);
+            $("#" + idArticulo).slideDown(1500);
+        })*/
+
     });
+    var articuloActivo; //Mientras esté fuera de la función no influye la posición,
+                        //puede ser arriba o abajo.
 
-    $(".articulo").append("<button class='cambia'>Cambia visualización</button>");
-    $(".cambia").click(function() {
-        $("article").hide();
-
+    $("article").each(function (index) {
+        console.log(index)
+        $(this).append("<button id='cambia" + index + "'>Ocultar</button>");
+        $("#cambia" + index).click(function () {
+            $("#articulo_29juR_" + index).hide();
+        });
     });
-
-    
 });
-
