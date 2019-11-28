4bc42caa02a5951f17f0891fef6738ee7c548102
Aurea <aurea.mene.h@gmail.com>
2019-11-28 18:03:49 +0100

    Empezando Angular, ejemplos de Node y de TypeScript

---
+++ 09_Angular/01_Node/servidor.js
@@ -0,0 +1,7 @@
+console.log("Hola, bienvenidos a mi programa");
+
+console.log("Dir: " + process.cwd());
+console.log("PID: " + process.pid);
+console.log("Platform: " + process.platform);
+
+console.log("ppid: " + process.ppid);
